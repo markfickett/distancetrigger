382036c8ca732539a6476b4a3f121949a48b5168
Mark Fickett <mark.fickett@gmail.com>
2013-01-31 00:35:21 -0500

    Trigger pin for opto-isolator (to camera). Adjust constants.

--- distancetrigger.ino
+++ distancetrigger.ino
@@ -4,11 +4,12 @@
  */
 
 #define PIN_LED		13
+#define PIN_TRIGGER	12
 #define PIN_DISTANCE	A0
-#define THRESHOLD	6
+#define THRESHOLD_F	0.15	// Percent change in distance that will trigger.
+#define DISTANCE_MAX	300	// Maximum distance to register.
 #define SERIAL_BAUD	9600
-#define TRIGGER_COUNT	6
-#define TRIGGER_DELAY	20
+#define TRIGGER_DELAY	80
 #define SAMPLE_INTERVAL	500
 
 int lastDist;
@@ -16,28 +17,35 @@ int sampleClock;
 
 void setup() {
 	sampleClock = 0;
-	lastDist = analogRead(PIN_DISTANCE);
+	lastDist = min(DISTANCE_MAX, analogRead(PIN_DISTANCE));
 	pinMode(PIN_LED, OUTPUT);
+	pinMode(PIN_TRIGGER, OUTPUT);
+	digitalWrite(PIN_TRIGGER, LOW);
 	Serial.begin(SERIAL_BAUD);
 }
 
 void trigger() {
-	for (int i = 0; i < TRIGGER_COUNT; i++) {
-		digitalWrite(PIN_LED, HIGH);
-		delay(TRIGGER_DELAY);
-		digitalWrite(PIN_LED, LOW);
-	}
+	digitalWrite(PIN_LED, HIGH);
+	digitalWrite(PIN_TRIGGER, HIGH);
+	delay(TRIGGER_DELAY);
+	digitalWrite(PIN_TRIGGER, LOW);
+	digitalWrite(PIN_LED, LOW);
 }
 
 void loop() {
-	int dist = analogRead(PIN_DISTANCE);
+	int dist = min(DISTANCE_MAX, analogRead(PIN_DISTANCE));
+	float delta = float(abs(dist - lastDist))/lastDist;
 	if (sampleClock++ % SAMPLE_INTERVAL == 0) {
-		Serial.println(dist);
+		Serial.print(dist);
+		Serial.print("\t");
+		Serial.println(delta);
 	}
-	int delta = abs(dist - lastDist);
-	if (delta >= THRESHOLD) {
+	if (delta >= THRESHOLD_F) {
 		Serial.print("Triggered at ");
-		Serial.println(dist);
+		Serial.print(dist);
+		Serial.print(" (");
+		Serial.print(delta);
+		Serial.println(").");
 		lastDist = dist;
 		trigger();
 	}
