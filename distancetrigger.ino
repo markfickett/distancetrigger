428da811f71f774a2c4e6bf31363363d0d85a525
Mark Fickett <mark.fickett@gmail.com>
2013-03-04 00:14:56 -0500

    Rework for beam-break sensor (with laser and light-level sensor).

--- distancetrigger.ino
+++ distancetrigger.ino
@@ -1,21 +1,18 @@
 /**
- * Trigger a wired remote (via opto-isolator) in response to distance change
- * (sensed with analog input from a hypersonic rangefinder).
+ * Trigger a wired remote (via opto-isolator) in response to breaking a beam
+ * (sensed with analog input from an ambient light sensor).
  */
 
 #define PIN_LED			13
 #define PIN_TRIGGER		12
-#define PIN_DISTANCE		A0
-#define THRESHOLD_F		0.55	// %Delta-distance that will trigger.
-#define THRESHOLD_DIST		180	// Distances greater than this trigger.
-#define DISTANCE_MAX		300	// Maximum distance to register.
+#define PIN_BEAM		A0
+#define THRESHOLD		250	// Brightness less than this triggers.
 #define SERIAL_BAUD		9600
 #define TRIGGER_DELAY		0
 #define TRIGGER_DURATION	80
-#define SAMPLE_INTERVAL		200
-#define SMOOTHING_WINDOW	2
+#define SAMPLE_INTERVAL		1500
+#define SMOOTHING_WINDOW	1
 
-int lastDist;
 int sampleClock;
 int averages[SMOOTHING_WINDOW];
 
@@ -24,7 +21,6 @@ void setup() {
 		averages[i] = 0;
 	}
 	sampleClock = 0;
-	lastDist = min(DISTANCE_MAX, analogRead(PIN_DISTANCE));
 	pinMode(PIN_LED, OUTPUT);
 	pinMode(PIN_TRIGGER, OUTPUT);
 	digitalWrite(PIN_TRIGGER, LOW);
@@ -41,24 +37,23 @@ void trigger() {
 }
 
 void loop() {
-	int rawDist = min(DISTANCE_MAX, analogRead(PIN_DISTANCE));
-	averages[sampleClock % SMOOTHING_WINDOW] = rawDist;
-	int dist = 0;
+	int rawLevel = analogRead(PIN_BEAM);
+	averages[sampleClock % SMOOTHING_WINDOW] = rawLevel;
+	int level = 0;
 	for (int i = 0; i < SMOOTHING_WINDOW; i++) {
-		dist += averages[i];
+		level += averages[i];
 	}
-	dist /= SMOOTHING_WINDOW;
+	level /= SMOOTHING_WINDOW;
 	if (sampleClock++ % SAMPLE_INTERVAL == 0) {
-		Serial.print(rawDist);
+		Serial.print(rawLevel);
 		Serial.print("\t");
-		Serial.println(dist);
+		Serial.println(level);
 	}
-	if (dist > THRESHOLD_DIST) {
-		Serial.print(rawDist);
+	if (level < THRESHOLD) {
+		Serial.print(rawLevel);
 		Serial.print("\t");
-		Serial.print(dist);
+		Serial.print(level);
 		Serial.println("\ttriggered");
-		lastDist = dist;
 		trigger();
 	}
 }
