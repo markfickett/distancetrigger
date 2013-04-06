c7d5a7d4be362eae581ca6fa9dd2de37da1513d3
Mark Fickett <mark.fickett@gmail.com>
2013-04-06 12:44:32 -0400

    Remove smoothing.

--- distancetrigger.ino
+++ distancetrigger.ino
@@ -5,53 +5,45 @@
 
 #define PIN_LED			13
 #define PIN_TRIGGER		12
-#define PIN_BEAM		A0
+#define PIN_BEAM		11
+#define PIN_BEAM_SENSE		A0
 #define THRESHOLD		650	// Brightness less than this triggers.
 #define SERIAL_BAUD		9600
-#define TRIGGER_DELAY		0
+//#define TRIGGER_DELAY		0
 #define TRIGGER_DURATION	80
 #define SAMPLE_INTERVAL		1500
-#define SMOOTHING_WINDOW	1
 
 int sampleClock;
-int averages[SMOOTHING_WINDOW];
 
 void setup() {
-	for (int i = 0; i < SMOOTHING_WINDOW; i++) {
-		averages[i] = 0;
-	}
 	sampleClock = 0;
 	pinMode(PIN_LED, OUTPUT);
+	pinMode(PIN_BEAM, OUTPUT);
+	digitalWrite(PIN_BEAM, HIGH);
 	pinMode(PIN_TRIGGER, OUTPUT);
 	digitalWrite(PIN_TRIGGER, LOW);
 	Serial.begin(SERIAL_BAUD);
 }
 
 void trigger() {
+	#ifdef TRIGGER_DELAY
 	delay(TRIGGER_DELAY);
-	digitalWrite(PIN_LED, HIGH);
+	#endif
 	digitalWrite(PIN_TRIGGER, HIGH);
+	digitalWrite(PIN_BEAM, LOW);
+	digitalWrite(PIN_LED, HIGH);
 	delay(TRIGGER_DURATION);
-	digitalWrite(PIN_TRIGGER, LOW);
 	digitalWrite(PIN_LED, LOW);
+	digitalWrite(PIN_BEAM, HIGH);
+	digitalWrite(PIN_TRIGGER, LOW);
 }
 
 void loop() {
-	int rawLevel = analogRead(PIN_BEAM);
-	averages[sampleClock % SMOOTHING_WINDOW] = rawLevel;
-	int level = 0;
-	for (int i = 0; i < SMOOTHING_WINDOW; i++) {
-		level += averages[i];
-	}
-	level /= SMOOTHING_WINDOW;
+	int level = analogRead(PIN_BEAM_SENSE);
 	if (sampleClock++ % SAMPLE_INTERVAL == 0) {
-		Serial.print(rawLevel);
-		Serial.print("\t");
 		Serial.println(level);
 	}
 	if (level < THRESHOLD) {
-		Serial.print(rawLevel);
-		Serial.print("\t");
 		Serial.print(level);
 		Serial.println("\ttriggered");
 		trigger();
